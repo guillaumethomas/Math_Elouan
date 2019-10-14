981c56e31347debd71bde6839aef3c10a61ae9a1
Guillaume Thomas <guillaumethomas@outlook.com>
2019-10-14 07:43:30 -0700

    menu

--- menu.py
+++ menu.py
@@ -6,14 +6,23 @@ def print_classes(max_int):
     """Using reflection in python"""
     for name, obj in inspect.getmembers(sys.modules[__name__]):
         if inspect.isclass(obj):
-            print(obj.__doc__)
+            # print(obj.__doc__)
             dic[obj.__name__] = obj(max_int, "Int")
 
     return dic
 
-if __name__ == "__main__":
-    dic = print_classes(50)
+def ui(max_int):
+    print("\nOn aime les Maths!\nPar Guillaume THOMAS (Papa)\nPour Elouan\n2019\n")
+
+    # Reflection to generate menu:
+    dic = print_classes(max_int)
 
     for value in dic.values():
         print(value.type)
-        print(value.pr_case())  
+        print(value.pr_case())
+
+    return dic
+
+
+if __name__ == "__main__":
+    dic = ui(500)
